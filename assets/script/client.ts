3d92ae5a6ea46a1fb1159eb7f0ee16b9fe16854e
ashuralyk <ashuralyk@live.com>
2022-08-02 17:32:20 +0800

    feat: tested client rpc module and fix some bugs

--- assets/script/client.ts
+++ assets/script/client.ts
@@ -5,10 +5,6 @@ interface WinLoseCount {
     lose_count: number,
 }
 
-interface TransactionHash {
-    digest: string
-}
-
 interface Outpoint {
     tx_hash: string,
     index: number,
@@ -17,10 +13,6 @@ interface Outpoint {
 interface NFT {
     nfts: Array<number>,
     outpoint: Outpoint, // 这个不用使用
-}
-
-interface NFTs {
-    data: Array<NFT>
 }
 
 export default class Client {
@@ -47,22 +39,29 @@ export default class Client {
         });
     }
 
-    private async send_transaction(call_func: string): Promise<any> {
+    private async send_transaction(call_func: string): Promise<String> {
         let response = await this.request('make_request_digest', {
             sender: this.address,
             contract_call: call_func,
-            private_key: this.privkey
+            private_key: this.privkey,
+            payment: String(20)
         });
+        console.log(response);
         if (response.status != 200) {
             throw 'bad jsonrpc call';
         }
-        return response.data.result.digest;
+        return response.data.result;
     }
 
     // call while `claim` button clicked in achivement page
-    private async claim_nfts(): Promise<any> {
+    private async claim_nfts(): Promise<String> {
         let global = await this.fetch_global();
-        if (global.nfts.length > 0) {
+        let claimable = false;
+        for (let lock_hash of Object.keys(global.users)) {
+            let v = global.users[lock_hash];
+            claimable = v.nfts.length > 0;
+        }
+        if (claimable) {
             return await this.send_transaction('claim_nfts()');
         } else {
             return 'nothing';
@@ -74,26 +73,29 @@ export default class Client {
         let response = await this.request('fetch_global_data', {
             address: this.address,
         });
+        console.log(response);
         if (response.status != 200) {
             throw 'bad jsonrpc call';
         }
-        return JSON.parse(response.data.result.data);
+        return JSON.parse(response.data.result);
     }
 
     // call when win the battle
-    public async battle_win(): Promise<TransactionHash> {
+    public async battle_win(): Promise<String> {
         return await this.send_transaction('battle_win()');
     }
 
     // call when lose the battle
-    public async battle_lose(): Promise<TransactionHash> {
+    public async battle_lose(): Promise<String> {
         return await this.send_transaction('battle_lose()');
     }
 
     public async get_win_lose_count(): Promise<WinLoseCount> {
         let global = await this.fetch_global();
+        console.log('global =', global);
         let result = { win_count: 0, lose_count: 0 };
-        for (let v of global.nfts) {
+        for (let lock_hash of Object.keys(global.users)) {
+            let v = global.users[lock_hash];
             result = {
                 win_count: v.win_count,
                 lose_count: v.lose_count
@@ -103,16 +105,26 @@ export default class Client {
     }
 
     // call to fetch nfts owned by player
-    public async fetch_nfts(): Promise<NFTs> {
-        let hash = await this.claim_nfts();
-        console.log('claim_nfts hash =', hash);
-        let response = await this.request('fetch_personal_data', {
-            address: this.address,
-        });
-        if (response.status != 200) {
-            throw 'bad jsonrpc call';
+    // public async fetch_nfts(): Promise<Array<NFT>> {
+    //     let hash = await this.claim_nfts();
+    //     console.log('claim_nfts hash =', hash);
+    //     let response = await this.request('fetch_personal_data', this.address);
+    //     console.log(response);
+    //     if (response.status != 200) {
+    //         throw 'bad jsonrpc call';
+    //     }
+    //     return response.data.result;
+    // }
+
+    // call to fetch nfts owned by player
+    public async fetch_nfts(): Promise<Array<number>> {
+        let global = await this.fetch_global();
+        let result = [];
+        for (let lock_hash of Object.keys(global.users)) {
+            let v = global.users[lock_hash];
+            result = v.nfts;
         }
-        return JSON.parse(response.data.result.data);
+        return result;
     }
 }
 
@@ -121,7 +133,7 @@ export default class Client {
  * 
  *  let client = new Client(
  *      'http://127.0.0.1:8090',
- *      '8d929e962f940f75aa32054f19a5ea2ce70ae30bfe4ff7cf2dbed70d556265df',
+ *      '0x8d929e962f940f75aa32054f19a5ea2ce70ae30bfe4ff7cf2dbed70d556265df',
  *      'ckt1qyq93wzur9h9l6qwyk6d4dvkuufp6gvl08aszz5syl'
  *  );
  *  let tx_hash = await client.battle_win();
